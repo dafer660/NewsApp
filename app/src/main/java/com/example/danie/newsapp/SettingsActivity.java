eba013c8224dcb06e6eb5a1cf9b2dc6bda8dd3fc
Daniel Ferreira <danielf18@hotmail.com>
2018-06-02 19:04:09 +0100

    Added Fragment and Settings Menu Bar

---
+++ app/src/main/java/com/example/danie/newsapp/SettingsActivity.java
@@ -0,0 +1,65 @@
+package com.example.danie.newsapp;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    public static class NewsPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.activity_settings_main);
+
+            Preference orderBy = findPreference(getString(R.string.settings_orderby_key));
+            Preference section = findPreference(getString(R.string.settings_section_key));
+            Preference query = findPreference(getString(R.string.settings_query_key));
+
+            bindPreferenceSummaryToValue(orderBy);
+            bindPreferenceSummaryToValue(section);
+            bindPreferenceSummaryToValue(query);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+    }
+}
